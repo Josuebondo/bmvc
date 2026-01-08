53032dded9601d8bb055dc1d7826ae372b909c1e
JOSUE BONDO <josuebondojw@gmail.com>
2026-01-08 14:58:42 +0100

    Fix typo in Router usage in web.php

--- routes/web.php
+++ routes/web.php
@@ -9,7 +9,7 @@
 use Core\Routeur;
 
 // Route accueil
-Router::obtenir('/', 'AccueilControlleur@index')->nom('accueil');
+Routeur::obtenir('/', 'AccueilControlleur@index')->nom('accueil');
 
 // Route page de démarrage
 Routeur::obtenir('/demarrage', 'DémarrageControlleur@index')->nom('démarrage');
